5e0b249b294c08951a7755339adf4b3cd8eb479d
Build-coder <59242259+Build-coder@users.noreply.github.com>
2020-01-26 02:35:39 -0500

    Update and rename csv_handler.py to calculate_growth.py

--- calculate_growth.py
+++ calculate_growth.py
@@ -1,3 +1,4 @@
+
 import csv
 
 def get_last_days():
@@ -35,6 +36,8 @@ def monthly_change(last_days):
 
         csv_reader = csv.DictReader(csv_file)
 
+        adj_close = []
+
         for line in csv_reader:
 
             date = line['Date'].split('-')
@@ -44,20 +47,55 @@ def monthly_change(last_days):
 
             if day == last_days[0]:
 
-                print(line)
+                print(line['Adj Close'])
+                #print(line['Avj Close'])
                 print()
 
+                adj_close.append(float(line['Adj Close']))
+
                 last_days.pop(len(last_days)*-1)
-
-                print(last_days)
-
-
-
 
 
                 if last_days == []:
 
                     break
+
+    value_change = calculate_change(adj_close)
+
+    csv_file.close()
+
+    return value_change
+
+
+
+def calculate_change(adj_close):
+
+    value_change = []
+
+    for price in range(len(adj_close)-1):
+
+        difference = adj_close[1] - adj_close[0]
+        pre_percentage = difference / adj_close[0]
+        percentage_change = pre_percentage * 100
+
+        value_change.append(percentage_change)
+
+        adj_close.pop(len(adj_close)*-1)
+
+        #print(adj_close)
+
+    return value_change
+
+
+def print_change(change)
+
+    for price in change:
+
+
+
+
+
+
 
 if __name__ == '__main__':
 
@@ -65,6 +103,9 @@ if __name__ == '__main__':
 
     lst = get_last_days()
 
-    print(lst)
+    #print(lst)
 
-    monthly_change(lst)
+    change = monthly_change(lst)
+
+    print(change)
+
